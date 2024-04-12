1b7ef5c15ccb4faa30dadfa8b42a56b322443b4f
rails101 <1019482178@qq.com>
2024-04-12 10:05:21 +0800

    implement groups#update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,22 +4,40 @@ class GroupsController < ApplicationController
 
   end
 
+
+  def show
+    @group =Group.find(params[:id])
+
+
+  end
+   def edit
+    @group = Group.find(params[:id])
+   end
+
   def new
     @group = Group.new
 
   end
 
+  def create
+    @group = Group.new(group_params)
+    @group.save
 
-def create
-  @group = Group.new(group_params)
-  @group.save
+    redirect_to groups_path
 
-  redirect_to groups_path
+  end
 
-end
-private
+  def update
+    @group = Group.find(params[:id])
+    @group.update(group_params)
+    redirect_to group_path,notice:"Update Success"
 
-def group_params
+  end
+
+  private
+
+  def group_params
   params.require(:group).permit(:title,:description)
+
 end
 end
